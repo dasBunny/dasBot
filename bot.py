dde0001181a65affcf6c085d24ba14199266a1e1
tibequadorian <tibequadorian@posteo.de>
2020-05-06 22:48:50 +0200

    loul uncommittete changes die nicht im origin sind - merge go brrrr

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ import _pickle as pickle
 from datetime import datetime, timedelta
 from logins import bot_token
 from discord.ext import commands
-from strings import *
-from config import *
+from strings import * #pylint: disable=unused-wildcard-import
+from config import *    #pylint: disable=unused-wildcard-import
 
 bot = commands.Bot(command_prefix='!', description='''Ich stinke hart nach hurensohn''')
 vote_list = []
@@ -43,8 +43,6 @@ async def on_message(message):
     await bot.process_commands(message)
     if message.channel.id == voting_channel_id:
         if message.author != bot.user:
-            print(message.author.name)
-            print(bot.user.name)
             print("deleting")
             await message.delete()
 
@@ -76,16 +74,16 @@ async def contest(ctx,message_id,*text):
 @bot.command()
 async def purge_votes(ctx):
     if vote_helper.checkForRole(ctx.author,'Admin'):
-        x = [None]
-        pickle.dump(x, open("votes.pickle","wb"))
         await bot.get_channel(voting_channel_id).purge()
+        vote_list = [] #pylint: disable=unused-variable 
+        save()
 
 @bot.command()
-async def vote(ctx,*topic):
+async def vote(ctx,majority,quota,*topic):
     if vote_helper.checkForRole(ctx.author,'Trusted'):
         topic_string = " ".join(topic)
         eligible = vote_helper.getEligibleCount(ctx)
-        v = voteClass.Vote(ctx.message.author.id,round((eligible*0.35)),round((eligible/2)+0.5),datetime.now()+timedelta(days=2),bot,topic_string)
+        v = voteClass.Vote(ctx.message.author.id,round((eligible*float(quota))),round(eligible*float(majority)),datetime.now()+timedelta(days=2),bot,topic_string)
         await v.sendMessage()
         vote_list.append(v)
         print(v.json_dict())
@@ -93,6 +91,24 @@ async def vote(ctx,*topic):
         #pickle.dump(vote_list, open("votes.pickle","wb"))
     else:
         await ctx.author.send(vote_no_permission_start)
+
+@bot.command()
+async def clear(ctx):
+    pass
+
+@bot.command()
+async def remove(ctx,message_id):
+    print(message_id)
+    if ctx.channel.id == voting_channel_id:
+        if vote_helper.checkForRole(ctx.author,'Admin'):
+            msg = await ctx.channel.fetch_message(message_id)
+            await msg.delete()
+            for v in vote_list:
+                print(v.message_id)
+                if v.message_id == msg.id:
+                    vote_list.remove(v)
+                    print('removed msg')
+                    save()
 
 def save():
     json_list = []
