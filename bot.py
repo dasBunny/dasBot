b94f1f98bc2ca58a9bf425bbf405251694b87e54
tibequadorian <tibequadorian@posteo.de>
2020-09-17 14:10:03 +0200

    create chats on demand and delete if unused

--- bot.py
+++ bot.py
@@ -3,23 +3,38 @@
 import discord
 from logins import bot_token
 
+
 class Bot(discord.Client):
-    def get_chat(self, channel):
-        return {
-            701916157149446245: 701916194135081070,
-            701916494300315669: 701933243485782067,
-            701921846228025387: 701921872924770354,
-        }[channel.id]
-    
+    chat = {}
+
     async def on_voice_state_update(self, member, before, after):
         if before.channel != after.channel:
+            # leave channel
             if before.channel is not None:
-                chat = self.get_channel(self.get_chat(before.channel))
-                await chat.send("{} left the channel".format(member.display_name))
+                chat = self.get_channel(self.chat[before.channel.id])
+                # remove permission from member
                 await chat.set_permissions(member, overwrite=None)
+                # delete text channel if empty
+                if not before.channel.members:
+                    self.chat.pop(before.channel.id, None)
+                    await chat.delete()
+            # join channel
             if after.channel is not None:
-                chat = self.get_channel(self.get_chat(after.channel))
+                # create new text channel if none exist
+                if after.channel.id not in self.chat or \
+                   self.get_channel(self.chat[after.channel.id]) is None:
+                    chat = await after.channel.guild .create_text_channel(
+                               'chat',
+                               topic=after.channel.name,
+                               category=self.get_channel(701908552163262508),
+                               position=2
+                           )
+                    self.chat[after.channel.id] = chat.id
+                # get associated text channel
+                else:
+                    chat = self.get_channel(self.chat[after.channel.id])
+                # give permission to member
                 await chat.set_permissions(member, read_messages=True)
-                await chat.send("{} joined the channel".format(member.display_name))
+
 
 Bot().run(bot_token)
